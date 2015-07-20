e77bf934fd2fc7be1f8066a3452da7bfc1921eaa
David Karapetyan <dkarapetyan@prescriptivedata.io>
2015-07-20 14:02:18 -0400

    Split analysis generation files into two components--steam and everything else

--- custom_fncs/sarima_analysis_everything_else.py
+++ custom_fncs/sarima_analysis_everything_else.py
@@ -1,6 +1,7 @@
-directory = '/Users/davidkarapetyan/Documents/workspace/data_analysis/'
-csv_file = ''
+# coding: utf-8
 
+
+from IPython import get_ipython
 import pandas as pd
 # for wide terminal display of pandas dataframes
 pd.options.display.width = 120
@@ -10,11 +11,15 @@ import statsmodels.tsa.arima_model as arima
 import statsmodels.tsa.statespace.sarimax as sarimax
 
 # plot inline
-# %pylab inline
+get_ipython().magic('pylab inline')
 # IPython.get_ipython().magic('matplotlib inline')
 import matplotlib.pylab as pylab
 
 pylab.rcParams['figure.figsize'] = 14, 6
+
+directory = '/Users/davidkarapetyan/Documents/workspace/data_analysis/'
+csv_file = 'data/park345_CHLR1.csv'
+
 
 
 # load dataframe, and subset out relevant columns
@@ -30,30 +35,27 @@ park_data = park_data.sort('TIMESTAMP')
 park_ts = pd.Series(list(park_data.VALUE),
                     pd.DatetimeIndex(park_data.TIMESTAMP),
                     name="steam values")
-# park_ts.drop_duplicates(inplace=True)
+
 park_ts = park_ts.loc[park_ts != 0].resample('15Min ').interpolate()
 print(park_ts)
 
 '''
-'''
 
 print(arima.ARIMA(park_ts, (0, 1, 0)).fit().summary())
 
-'''
-We see that ARIMA is not currently fitting the time series data. We look for
-an appropriate transformation of the time series to improve our ARIMA fitting.
 
-###Log Ratio Transformation
+# We see that ARIMA is not currently fitting the time series data. We look for
+# an appropriate transformation of the time series to improve our ARIMA fitting.
+# 
+# ###Log Ratio Transformation
+
+# To get a properly scaled plot, we filter out the outlier values occuring at
+# the end of day (shift from some steam usage to none at all as systems
+# restart, and spikes from ramp-up time at the beginning of the day).
+
 '''
 
 park_ts_logr = (np.log(park_ts / park_ts.shift(1)))[1:]
-
-'''
-To get a properly scaled plot, we filter out the outlier values occuring at
-the end of day (shift from some steam usage to none at all as systems
-restart, and spikes from ramp-up time at the beginning of the day).
-'''
-
 print(park_ts_logr.describe(percentiles=[0.05, 0.95]))
 
 park_ts_logr[(park_ts_logr > 0.001) & (park_ts_logr < 0.23)][
@@ -62,6 +64,7 @@ park_ts_logr[(park_ts_logr > 0.001) & (park_ts_logr < 0.23)][
 '''
 The seasonality is clear. We now plot a single day, filtering out
 outliers to get a properly scaled figure.
+
 '''
 
 park_ts_logr[(park_ts_logr > 0.005) & (park_ts_logr < 0.18)][
@@ -71,6 +74,7 @@ park_ts_logr[(park_ts_logr > 0.005) & (park_ts_logr < 0.18)][
 Next, we utilise a SARIMAX model, with seasonality at 96
 (our data points are spaced at 15 minute intervals),
 and analyze a week's worth of data (starting on Monday, and ending on Friday).
+
 '''
 
 print(sarimax.SARIMAX(park_ts_logr.loc['2013-05-06':'2013-05-10'],
@@ -83,6 +87,7 @@ the ratio of chilled water temperature dipping suddenly at the end of day
 as systems are ramped down.
 Observe that the only negative values in the data occur at the start of
 day. We filter these out, and re-run SARIMA.
+
 '''
 
 print(sarimax.SARIMAX(
@@ -90,10 +95,12 @@ print(sarimax.SARIMAX(
     seasonal_order=(0, 1, 0, 95)).fit().summary())
 
 '''
-Observe that the sarimax model seems to fit nicely now that we have
-filtered out the beginning-of-day spike. Now, let's use a larger input
+Our data is clustered very close to the mean--i.e, the spikes are very small spikes. Consequently, the positive of filtering them out (i.e. smoothing the data) outweighs the drawback of reducing the number of points to fit.
+
+Now, let's use a larger input
 (beginning on a Monday, and ending on a Friday), and fit another
 Sarimax model to our beginning-of-day spike-filtered data.
+
 '''
 
 print(sarimax.SARIMAX(
@@ -101,9 +108,10 @@ print(sarimax.SARIMAX(
     seasonal_order=(0, 1, 0, 95)).fit().summary())
 
 '''
-This is an even better fit than the fit for the week's worth of data.
+As expected, this is an even better fit than the fit for the week's worth of data.
 Lastly, we input three # months worth of data, beginning on a Monday,
 and ending on a Friday.
+
 '''
 
 print(sarimax.SARIMAX(park_ts_logr[park_ts_logr > 0]['2013-05-06': '2013-08-08'],
@@ -117,69 +125,49 @@ print(sarimax.SARIMAX(park_ts_logr['2013-05-06': '2013-08-08'],
                       seasonal_order=(0, 1, 0, 96)).fit().summary())
 
 '''
-The excellent fit for even a large data set after filtering the ramp-up
-and ramp-down 15 minute spikes, and the poor fit when we keep them in,
-suggests that, apart from ramp-up and ramp-down times, the behavior of
-chilled water temperate is strongly linear. A possible future line of
-research is to investigate seasonality on a monthly level (for example,
-analyzing data in chunks of winter, spring, summer, fall), and so on, in
-order to attempt further improve fit.
 
-However, from the looks of the data, it makes sense to keep the analysis of
-15-minute ramp-up and ramp-down times separate from all other data.
-'''
-
-'''
-##SARIMAX on Data for Individual Days
+Hence, it makes sense to keep the analysis of
+15-minute ramp-up and ramp-down times together with the analysis of the remaining data.
 
 We next investigate seasonality on a weekly basis. That is, we
 isolate the 5-day workweek into 5 chunks, and run SARIMAX on
 each chunk separately.
+
 '''
 
 print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 0,
                                                   park_ts_logr > 0)],
                       seasonal_order=(0, 1, 0, 95)).fit().summary())
 
-'''
-As expected, the fit is good, but not as good as for our data set considering
-all days of the week. We suspect that this is due to there being fewer data
-points in the SARIMAX fitting above. To remedy this, we re-run SARIMAX on a
-larger data set of Mondays.
-'''
-
-print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 0,
-                                                  park_ts_logr > 0)][
-                      '2013-03':'2014-02'],
-                      seasonal_order=(0, 1, 0, 95)).fit().summary())
-
-'''
-We do the same for Tuesdays, Wednesdays, etc.
-'''
-
 print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 1,
-                                                  park_ts_logr > 0)][
-                      '2013-03':'2014-02'],
+                                                  park_ts_logr > 0)],
                       seasonal_order=(0, 1, 0, 95)).fit().summary())
-
-
-
-
-# SARIMAX(0,1,0,95) MLE estimator doesn't converge
 
 print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 2,
-                                                  park_ts_logr > 0)][
-                      '2013-03':'2014-02'],
+                                                  park_ts_logr > 0)],
                       seasonal_order=(0, 1, 0, 95)).fit().summary())
 
 print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 3,
-                                                  park_ts_logr > 0)][
-                      '2013-03':'2014-02'],
+                                                  park_ts_logr > 0)],
                       seasonal_order=(0, 1, 0, 95)).fit().summary())
 
 print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 4,
-                                                  park_ts_logr > 0)][
-                      '2013-03':'2014-02'],
+                                                  park_ts_logr > 0)],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 0],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 1],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 2],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 3],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 4],
                       seasonal_order=(0, 1, 0, 95)).fit().summary())
 
 '''
@@ -187,6 +175,7 @@ print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 
 
 We shall treat Monday ramp-ups separately from the remaining days of the week,
 due to difference between system-idling over weekends and weekdays.
+
 '''
 
 print(arima.ARIMA(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 0,
@@ -203,12 +192,9 @@ volatility, since $log(x)$ is an exponential function of $x$ for decreasing
 $x < 1$. This will result in decreasing the goodness-of-fit of a linear
 model, from a least-squares standpoint. Consequently, we exponentiate our
 time series data, and re-fit:
+
 '''
 
 print(arima.ARIMA(np.exp(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 0,
                                                      park_ts_logr < 0)]),
                   order=(0, 1, 0)).fit().summary())
-
-'''
-Observe that the fit has improved dramatically.
-'''
---
+++ custom_fncs/sarima_analysis_steam.py
@@ -0,0 +1,207 @@
+# coding: utf-8
+
+import pandas as pd
+# for wide terminal display of pandas dataframes
+pd.options.display.width = 120
+pd.options.display.max_rows = 10000
+import numpy as np
+import statsmodels.tsa.arima_model as arima
+import statsmodels.tsa.statespace.sarimax as sarimax
+from IPython import get_ipython
+
+# plot inline
+get_ipython().magic('pylab inline')
+# IPython.get_ipython().magic('matplotlib inline')
+import matplotlib.pylab as pylab
+
+pylab.rcParams['figure.figsize'] = 14, 6
+
+directory = '/Users/davidkarapetyan/Documents/workspace/data_analysis/'
+csv_file = 'data/park345_steam.csv'
+
+
+# load dataframe, and subset out relevant columns
+park_data = pd.read_csv(directory + csv_file, error_bad_lines=False)
+park_data.columns = ['ID', 'TIMESTAMP',
+                     'TRENDFLAGS', 'STATUS',
+                     'VALUE', 'TRENDFLAGS_TAG',
+                     'STATUS_TAG']
+park_data = park_data.sort('TIMESTAMP')
+
+
+# construct time series, getting rid of microseconds
+park_ts = pd.Series(list(park_data.VALUE),
+                    pd.DatetimeIndex(park_data.TIMESTAMP),
+                    name="steam values")
+
+park_ts = park_ts.loc[park_ts != 0].resample('15Min ').interpolate()
+print(park_ts)
+
+
+
+print(arima.ARIMA(park_ts, (0, 1, 0)).fit().summary())
+
+'''
+We see that ARIMA is not currently fitting the time series data. We look for an appropriate transformation of the time series to improve our ARIMA fitting.
+
+###Log Ratio Transformation
+
+To get a properly scaled plot, we filter out the outlier values occuring at
+the end of day (shift from some steam usage to none at all as systems
+restart, and spikes from ramp-up time at the beginning of the day).
+
+'''
+
+park_ts_logr = (np.log(park_ts / park_ts.shift(1)))[1:]
+basic_stats = park_ts_logr.describe(percentiles=[0.05, 0.95])
+
+print(basic_stats)
+park_ts_logr[park_ts_logr.between(basic_stats['5%'],
+                                  basic_stats['95%']
+                                  )]['2013-05-01': '2013-05-15'].plot()
+
+'''
+
+The seasonality is clear. We now plot a single day, filtering out
+outliers to get a properly scaled figure.
+
+'''
+
+park_ts_logr[park_ts_logr.between(basic_stats['5%'],
+                                  basic_stats['95%']
+                                  )]['2013-05-15'].plot()
+
+'''
+Next, we utilise a SARIMAX model, with seasonality at 96
+(our data points are spaced at 15 minute intervals),
+and analyze a week's worth of data (starting on Monday, and ending on Friday).
+
+'''
+
+print(sarimax.SARIMAX(park_ts_logr.loc['2013-05-06':'2013-05-10'],
+                      seasonal_order=(0, 1, 0, 96)).fit().summary())
+
+'''
+While the fit isn't terrible, it perhaps can be improved by first observing
+that we have spikes in our at the beginning of the day. This is due to
+the ratio of consecutive values dipping suddenly at the end of day
+as systems are ramped down.
+Observe that the only negative values in the data occur at the start of
+day. We filter these out, and re-run SARIMA.
+
+'''
+
+print(sarimax.SARIMAX(
+    park_ts_logr[park_ts_logr > basic_stats['5%']]['2013-05-06':'2013-05-10'],
+    seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+'''
+The positives of smoothing the data via filtration have been outweighed by the loss of data points to fit.
+
+Now, let's use a larger input
+(beginning on a Monday, and ending on a Friday), and fit another
+Sarimax model to our beginning-of-day spike-filtered data.
+
+'''
+
+print(sarimax.SARIMAX(
+    park_ts_logr[park_ts_logr > 0]['2013-05-06':'2013-06-07'],
+    seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+'''
+As expected, this is an even better fit than the fit for the week's worth of data.
+Lastly, we input three # months worth of data, beginning on a Monday,
+and ending on a Friday.
+
+'''
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr > 0]['2013-05-06': '2013-08-08'],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+'''
+Let's contrast this with our fit when we include the end-of-day spikes:
+
+'''
+
+print(sarimax.SARIMAX(park_ts_logr['2013-05-06': '2013-08-08'],
+                      seasonal_order=(0, 1, 0, 96)).fit().summary())
+
+'''
+Hence, it makes sense to keep the analysis of
+15-minute ramp-up and ramp-down times separate from the analysis of the remaining data.
+
+##SARIMAX on Data for Individual Days
+
+We next investigate seasonality on a weekly basis. That is, we
+isolate the 5-day workweek into 5 chunks, and run SARIMAX on
+each chunk separately.
+
+'''
+
+print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 0,
+                                                  park_ts_logr > basic_stats['5%'])],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 1,
+                                                  park_ts_logr > basic_stats['5%'])],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 2,
+                                                  park_ts_logr > basic_stats['5%'])],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 3,
+                                                  park_ts_logr > basic_stats['5%'])],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 4,
+                                                  park_ts_logr > basic_stats['5%'])],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 0],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 1],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 2],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 3],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+print(sarimax.SARIMAX(park_ts_logr[park_ts_logr.index.weekday == 4],
+                      seasonal_order=(0, 1, 0, 95)).fit().summary())
+
+'''
+##ARIMAX For Ramp-up and Ramp-Down
+
+We shall treat Monday ramp-ups separately from the remaining days of the week,
+due to difference between system-idling over weekends and weekdays.
+
+'''
+
+print(arima.ARIMA(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 0,
+                                              park_ts_logr < 0)],
+                  order=(0, 1, 0)).fit().summary())
+
+'''
+Observe that between end of day and start of day, chilled water temperature
+decreases, and so $$\frac{T_k(0 + 00:15)}{T_k(0)} = a_k < 1$$ for each
+entry in our training set. The set $\{a_k\}_k$ will hopefully consist of
+entries that are clustered near one another, with some reasonable volatility.
+Taking a logarithm of them will result in an exponential increase in
+volatility, since $log(x)$ is an exponential function of $x$ for decreasing
+$x < 1$. This will result in decreasing the goodness-of-fit of a linear
+model, from a least-squares standpoint. Consequently, we exponentiate our
+time series data, and re-fit:
+'''
+
+print(arima.ARIMA(np.exp(park_ts_logr[np.logical_and(park_ts_logr.index.weekday == 0,
+                                                     park_ts_logr < 0)]),
+                  order=(0, 1, 0)).fit().summary())
+
+'''
+Observe that the fit has improved.
+'''
