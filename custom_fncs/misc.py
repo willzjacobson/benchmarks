c64456fa15ecd9d32a8c0cf6e37def0395163a5b
David Karapetyan <dkarapetyan@prescriptivedata.io>
2015-07-24 18:13:46 -0400

    Miscellaneous functions refactored and stationarity test updated to test up to 10 lags

--- custom_fncs/misc.py
+++ custom_fncs/misc.py
@@ -1,38 +1,44 @@
-import statsmodels.tsa.arima_model as arima
-from statsmodels.tsa import stattools
-from scipy import optimize
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.stattools import adfuller
+from scipy.optimize import brute
 import pandas as pd
+
+
 # custom funcs
 
 
 # test for stationarity
 
-def stationarity_test( ts ):
-    if stattools.adfuller( ts )[1] > 0.05:
-        print( "May be non-stationary after taking first lag" )
+def stationarity_test(ts):
+    lower = 0
+    upper = 10
 
+    for i in range(lower, upper):
+        if adfuller(ts)[1] < 0.05:
+            return "Is stationary only after taking at least {} lags".format(str(i))
 
+    print("May not be stationary even after {}-{} lags".format(str(lower), str(upper)))
 
 
 # function to find optimal ARIMA order
 
-def arima_aic( endog, order ):
-    return arima.ARIMA( endog, order ).fit().aic
+def arima_aic(endog, order):
+    return ARIMA(endog, order).fit().aic
 
 
-def optimal_order( ts ):
-    return tuple( 
-        map( int,
-            optimize.brute( lambda x: arima_aic( ts, x ),
-                           ranges = ( slice( 0, 2, 1 ),
-                                   slice( 0, 2, 1 ),
-                                   slice( 0, 2, 1 ) ),
-                           finish = None )
+def optimal_order(ts):
+    return tuple(
+        map(int,
+            brute(lambda x: arima_aic(ts, x),
+                  ranges=(slice(0, 2, 1),
+                          slice(0, 2, 1),
+                          slice(0, 2, 1)),
+                  finish=None)
             )
     )
 
 
-def ts_day_pos( ts, day, start, end, freq ):
+def ts_day_pos(ts, day, start, end, freq):
     temp_ts = ts[ts > 0]
-    temp_ts = temp_ts[pd.date_range( start, end, freq )]
+    temp_ts = temp_ts[pd.date_range(start, end, freq)]
     return temp_ts[temp_ts.index.weekday == day]
