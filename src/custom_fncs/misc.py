b84acc96839dc7fc09d192e3b9685b53bd397d9f
David Karapetyan <dkarapetyan@prescriptivedata.io>
2015-08-17 17:15:29 -0400

    Working actual v prediction function plotter, with 2 std filtered out of prediction for pretty plot

--- src/custom_fncs/misc.py
+++ src/custom_fncs/misc.py
@@ -55,6 +55,12 @@ def ts_day_pos(ts, day, time, start, end, freq):
         return temp.at_time(time)
 
 
+def filter_two_std(ts):
+    stats = ts.describe(percentiles=[.05, .95])
+    low, high = stats['5%'], stats['95%']
+    return ts[ts.between(low, high)]
+
+
 def actual_vs_prediction(ts, order=(1, 1, 0), seasonal_order=(1, 1, 0, 96),
                          days=(0, 1, 2, 3, 4, 5, 6)):
     """Plots SARIMA predictions against real values for each weekday.
@@ -65,37 +71,45 @@ def actual_vs_prediction(ts, order=(1, 1, 0), seasonal_order=(1, 1, 0, 96),
     :param days: tuple
     :return: None
     """
-    if len(days) > 2:
-        ncols = int(np.ceil(len(days) / 2))
+    days_length = len(days)
+    if days_length > 2:
+        ncols = int(np.ceil(days_length / 2))
         nrows = 2
 
     else:
-        ncols = len(days)
+        ncols = days_length
         nrows = 1
 
     fig, ax = plt.subplots(nrows, ncols, squeeze=False)
 
-    if nrows*ncols > len(days):
-        fig.delaxes( ax[nrows - 1, ncols - 1]) # one more plot than needed
+    if nrows * ncols > days_length:
+        fig.delaxes(ax[nrows - 1, ncols - 1])  # one more plot than needed
 
     fig.suptitle('In-sample Prediction vs Actual')
     weekdays = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday',
                 4: 'Friday',
                 5: 'Saturday', 6: 'Sunday'}
 
-    for axentry in ax:
-        for i, j in zip(range(ncols), days):
-            ts_within_day = ts[ts.index.weekday == j]
-            fit = sarimax.SARIMAX(ts_within_day, order=order,
-                                  seasonal_order=seasonal_order).fit()
-            axentry[i].set_title(weekdays[j])
-            axentry[i].plot(fit.data.dates, fit.data.endog.flatten(),
-                            label='Actual')
-            axentry[i].plot(fit.data.dates, fit.predict().flatten(),
-                            label='Prediction')
-            axentry[i].legend(loc='best')
-            axentry[i].set_xlabel('Weekly Readings')
-            axentry[i].set_ylabel('Accumulated Steam Usage ')
+    days_iter = iter(days)
+    day = next(days_iter, None)
+    for axrow in ax:
+        for i in range(ncols):
+            if day != None:
+                ts_within_day = ts[ts.index.weekday == day]
+                fit = sarimax.SARIMAX(ts_within_day, order=order,
+                                      seasonal_order=seasonal_order).fit()
+                axrow[i].set_title(weekdays[day])
+                axrow[i].plot(ts.index, ts.values,
+                              label='Actual')
+                ts_fit = pd.Series(data=fit.predict().flatten(),
+                                   index=fit.data.dates)
+                ts_fit_filtered = filter_two_std(ts_fit)
+                axrow[i].plot(ts_fit_filtered.index, ts_fit_filtered.values,
+                              label='Prediction')
+                axrow[i].legend(loc='best')
+                axrow[i].set_xlabel('Weekly Readings')
+                axrow[i].set_ylabel('Accumulated Steam Usage ')
+                day = next(days_iter, None)
     plt.show()
 
 
@@ -152,13 +166,8 @@ def number_diff(ts, upper=10):
         "May not be stationary even after 0-{} lags".format(
             str(upper)))  # adfuller(park_ts_logr[
 
+
 # park_ts_logr.index.weekday == 7].at_time('11:00'), autolag='AIC')
 
 
 # def predict_start_time(ts, crit_time = '7:00:00'):
-
-
-def filter_two_std(ts):
-    stats = ts.describe(percentiles=[.05, .95])
-    low, high = stats['5%'], stats['95%']
-    return ts[ts.between(low, high)]
