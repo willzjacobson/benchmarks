9925ef4e2f54e92eb653542f8cffc72a8368be83
David Karapetyan <david.karapetyan@gmail.com>
2015-10-16 18:03:54 -0400

    Refactoring

--- src/data_clean.py
+++ src/data_clean.py
@@ -2,7 +2,7 @@
 # from IPython.parallel import Client
 # rc=Client()
 import os
-
+from joblib import Parallel, delayed
 import pandas as pd
 
 
@@ -30,6 +30,30 @@ def data_clean_park(data, title):
     return ts
 
 
+def _split_concat(file, root, sep="|"):
+    temp = pd.read_csv(root + "/" + file,
+                       header=None,
+                       sep=sep,
+                       dtype="object")
+
+    if file.count("_") > 1:
+        string_split = file.split("_", 2)
+        controller = string_split[0]
+        subcontroller = string_split[1]
+        pointname = string_split[2]
+    else:
+        string_split = file.split("_", 1)
+        controller = string_split[0]
+        subcontroller = float('NaN')
+        pointname = string_split[1]
+
+    # add new columns
+    temp.insert(0, 'Controller', controller)
+    temp.insert(1, 'SubController', subcontroller)
+    temp.insert(2, 'PointName', pointname)
+    return temp
+
+
 def csv_dir_to_hdf(path_to_dir="/", header=0, sep="\,"):
     for root, dirs, files in os.walk(path_to_dir):
 
@@ -45,34 +69,14 @@ def csv_dir_to_hdf(path_to_dir="/", header=0, sep="\,"):
         store.close()
 
 
-def csv_multicsv_to_hdf(path_to_dir, header=0, sep="\,"):
-    master, sorted_csvs, store = pd.DataFrame(), None, None
+def csv_multicsv_to_hdf(path_to_dir, sep="|"):
+    root, dirs, files = list(os.walk(path_to_dir))[0][0:3]
+    store = pd.HDFStore(root + ".h5")
 
-    for root, dirs, files in os.walk(path_to_dir):
-        store = pd.HDFStore(root + ".h5")
+    frames = (Parallel(n_jobs=-1)(delayed(_split_concat)(file, root, sep)
+                                  for file in files))
 
-        for file in files:
-
-            temp = pd.read_csv(root + "/" + file, header=header, sep=sep)
-
-            if file.count("_") < 2:
-                string_split = file.split("_", file.count("_"))
-                controller = string_split[0]
-                subcontroller = string_split[1]
-                pointname = string_split[2]
-            else:
-                string_split = file.split("_", 1)
-                controller = string_split[0]
-                subcontroller = float('NaN')
-                pointname = string_split[1]
-
-
-            # add new columns
-            temp.insert(0, 'Controller', controller)
-            temp.insert(1, 'SubController', subcontroller)
-            temp.insert(2, 'PointName', pointname)
-
-            master = pd.concat([master, temp])
-
-    store['df'] = master
-    store.close()
+    # master = pd.concat(frames, verify_integrity=True)
+    # store['df'] = master
+    # store.close()
+    return frames
--- src/start_up_weather.py
+++ src/start_up_weather.py
@@ -1,9 +1,16 @@
 import weather
-city = "New_York"
-state = "NY"
 
-weather = weather.archive_update(city, state)
-latest_forecast = weather.forecast(city, state)
+if __name__ == "__main__":
+    city = "New_York"
+    state = "NY"
+    archive_location = "../data/weather.h5"
+    df_history = "history"
+    df_forecast = "forecast"
+    cap = 9
 
-weather.to_hdf("data/weather.h5", "history")
-latest_forecast.to_hdf("data/weather.h5", "forecast")
+    weather_history = weather.archive_update(city, state, archive_location,
+                                             df_history,
+                                             cap)
+    latest_forecast = weather.forecast(city, state)
+    weather_history.to_hdf(archive_location, df_history)
+    latest_forecast.to_hdf(archive_location, df_forecast)
