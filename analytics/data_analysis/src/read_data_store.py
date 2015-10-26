d0df4e71356f573684b6b18cf97c91ac3bea5e99
ashishgagneja <agagneja@prescriptivedata.io>
2015-10-26 16:11:56 -0400

    bug-fix

--- analytics/data_analysis/src/read_data_store.py
+++ analytics/data_analysis/src/read_data_store.py
@@ -10,28 +10,31 @@ GROUPS_PICKLE_file = '/home/ashishgagneja/misc/groups.pickle'
 
 groups = None
 
-if not os.path.exists(GROUPS_PICKLE_file):
-
-    print('pickle file not found\n')
-
-    store = pd.HDFStore(HDF5_file)
-
-    # find distinct timeseries
-    df = store.select('df')
-    grouped = df.groupby(['Controller', 'Subcontroller', 'PointName'], sort=False)
-    groups = grouped.groups
-
-    store.close()
-
-else:
-    print('loading pickle\n')
-    with open(GROUPS_PICKLE_file, 'rb') as f:
-        groups = pickle.load(f)
+# if not os.path.exists(GROUPS_PICKLE_file):
+#
+#     print('pickle file not found\n')
+#
+#     store = pd.HDFStore(HDF5_file)
+#
+#     # find distinct timeseries
+#     df = store.select('df')
+#     grouped = df.groupby(['Controller', 'Subcontroller', 'PointName'], sort=False)
+#     groups = grouped.groups
+#
+#     store.close()
+#
+# else:
+#     print('loading pickle\n')
+#     with open(GROUPS_PICKLE_file, 'rb') as f:
+#         groups = pickle.load(f)
+print('loading pickle\n')
+with open(GROUPS_PICKLE_file, 'rb') as f:
+    groups = pickle.load(f)
 
 
 # write group keys
 for k, v in groups.iteritems():
     ctrlr, sub_cntrlr, pt_nm = k
-    print('%s,%s,%s' % (ctrlr, subcntrlr, pt_nm))
+    print('%s,%s,%s' % (ctrlr, sub_cntrlr, pt_nm))
 
 
