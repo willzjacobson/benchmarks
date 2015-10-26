0fe6a9720cd74a6267967595d0a03f3e3a91b6b3
ashishgagneja <agagneja@prescriptivedata.io>
2015-10-26 16:32:17 -0400

    bug-fix

--- analytics/data_analysis/src/read_data_store.py
+++ analytics/data_analysis/src/read_data_store.py
@@ -3,7 +3,7 @@ __author__ = 'ashishgagneja'
 import pandas as pd
 import pickle
 import os
-
+import json
 
 HDF5_file = '/data/park345_1a_1b.h5'
 GROUPS_PICKLE_file = '/home/ashishgagneja/misc/groups.pickle'
@@ -12,24 +12,26 @@ groups = None
 
 # if not os.path.exists(GROUPS_PICKLE_file):
 #
-#     print('pickle file not found\n')
-#
-#     store = pd.HDFStore(HDF5_file)
-#
-#     # find distinct timeseries
-#     df = store.select('df')
-#     grouped = df.groupby(['Controller', 'Subcontroller', 'PointName'], sort=False)
-#     groups = grouped.groups
-#
-#     store.close()
+
+#print('pickle file not found\n')
+store = pd.HDFStore(HDF5_file)
+
+# find distinct timeseries
+df = store.select('df')
+grouped = df.groupby(['Controller', 'Subcontroller', 'PointName'], sort=False)
+groups = grouped.groups
+
+with open('/home/ashishgagneja/misc/groups.json', 'wb+') as f:
+    json.dump(groups, f)
+
+store.close()
+
 #
 # else:
 #     print('loading pickle\n')
 #     with open(GROUPS_PICKLE_file, 'rb') as f:
 #         groups = pickle.load(f)
-print('loading pickle\n')
-with open(GROUPS_PICKLE_file, 'rb') as f:
-    groups = pickle.load(f)
+)
 
 
 # write group keys
