3ef84195a941afea8418339776c666a6c0acaed7
David Karapetyan <dkarapetyan@prescriptivedata.io>
2015-10-27 17:02:31 -0400

    Use config file as a module, instead of as json (cleaner loading into scripts)

--- analytics/TPO2-Rudin/config.py
+++ analytics/TPO2-Rudin/config.py
@@ -1,19 +1,32 @@
-{
-  "default": {
+david = dict(default={
     "base_dir": "/home/davidkarapetyan/Documents/workspace/Adirondack/analytics",
     "python_prog": "/home/davidkarapetyan/anaconda3/bin/python",
     "data_sources": "/data",
     "log_dir": "/var/log"
-  },
-  "weather": {
+}, weather={
     "wund_url": "http://api.wunderground.com/api/bab4ba5bcbc2dbec/",
     "table": "/data/weather.h5",
     "history": "history",
     "forecast": "forecast"
-  },
-  "sampling": {
+}, sampling={
     "forecast_granularity": 15,
     "forecast_length": 24
-  }
-}
+}, svm={
+    "gamma": 1,
+    "C": 1,
+    "cache_size": 10000,
+    "kernel": "rbf",
+    "max_iter": -1,
+    "prpobability": "False",
+    "random_state": "None",
+    "shrinking": "True",
+    "tol": "0.001",
+    "verbose": "False"
+}, sarima={
+    "order": "(1, 1, 0)",
+    "seasonal_order": "(1,1,3)",
+    "enforce_stationarity": "False"
+})
 
+ashish = None
+master = None
