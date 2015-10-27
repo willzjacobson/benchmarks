8c858f2d310d6a9b7e3817e2ed5a72988934cd35
anirbanray <aray@prescriptivedata.io>
2015-10-27 16:19:59 -0400

    using promise to return map

--- hardware/BACNET/map.js
+++ hardware/BACNET/map.js
@@ -1,37 +1,31 @@
 var parse = require('csv-parse'),
-    fs = require('fs')
+    fs = require('fs'),
+    Q = require('q'),
+    deferred = Q.defer();
 
 
 
 
-var spreadsheet = function() {
-        fs.readFile('Park345.csv', function(err, data) {
-            parse(data, function(err, data) {
-                return data;
-            })
-        })
-    }
     //asuming pointArr from readpropm will be [name, value]
 
 var mapper = function(pointArr) {
-    var maparr = spreadsheet();
-    for (var i = 0; i < maparr.length; i++) {
-        if (maparr[i][4] == pointArr[0]) {
-            return {
-                "buildingRoute": mapparr[i][0],
-                "BMSPointName": mapparr[i][1],
-                "Master": mapparr[i][2],
-                "Controller": mapparr[i][3],
-                "PointName": mapparr[i][4],
-                "System": mapparr[i][5],
-                "System display name": mapparr[i][6],
-                "Description": mapparr[i][7],
-                "Units": mapparr[i][8],
-                "Sample": mapparr[i][9]
+    fs.readFile('Park345.csv', function(err, data) {
+        parse(data, function(err, map) {
+            for (var i = 0; i < map.length; i++) {
+                if (map[i][4] == pointArr[0]) {
+                     deferred.resolve(map[i])
+                }
             }
-        }
-    }
+        })
+    })
+
+    return deferred.promise
 };
+
+
+mapper(['HouseTank45High', 345]).then(function(data){
+	console.log(data)
+})
 
 
 
