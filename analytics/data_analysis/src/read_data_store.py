6b9fa03e34c6837a193cae19f9c53e12ba824466
ashishgagneja <agagneja@prescriptivedata.io>
2015-10-28 16:21:08 -0400

    working version

--- analytics/data_analysis/src/read_data_store.py
+++ analytics/data_analysis/src/read_data_store.py
@@ -12,15 +12,12 @@ from joblib import Parallel, delayed
 HDF5_file = '/data/park345_1a_1b.h5'
 GROUPS_PICKLE_file = '/home/ashishgagneja/misc/groups.pickle'
 
-
-groups = None
-
-
-def process_key(key):
-    ctrlr, sub_cntrlr, pt_nm = key
-    #print('%s:%s:%s\n' % (ctrlr, sub_cntrlr, pt_nm ))
-    tmp_df = df[(df.Controller == ctrlr) & (df.Subcontroller == sub_cntrlr) & (df.PointName == pt_nm)]
-    return [key, tmp_df.size]
+#
+# def process_key(key):
+#     ctrlr, sub_cntrlr, pt_nm = key
+#     #print('%s:%s:%s\n' % (ctrlr, sub_cntrlr, pt_nm ))
+#     tmp_df = df[(df.Controller == ctrlr) & (df.Subcontroller == sub_cntrlr) & (df.PointName == pt_nm)]
+#     return [key, tmp_df.size]
 
 
 # if not os.path.exists(GROUPS_PICKLE_file):
@@ -31,23 +28,23 @@ store = pd.HDFStore(HDF5_file)
 
 # find distinct timeseries
 df = store.select('df')
-#grouped = df.groupby(['Controller', 'Subcontroller', 'PointName'], sort=False)
+grouped = df.groupby(['Controller', 'Subcontroller', 'PointName'], sort=False)
 #groups = grouped.groups
 
-controller_list = pd.unique(df['Controller'].ravel())
-controller_list = np.delete(controller_list, np.nan)
-
-subcontroller_list = pd.unique(df['Subcontroller'].ravel())
-subcontroller_list = np.delete(subcontroller_list, np.nan)
-
-pointname_list = pd.unique(df['PointName'].ravel())
-pointname_list = np.delete(pointname_list, np.nan)
-
-cross_product = itertools.product(controller_list, subcontroller_list, pointname_list)
-
-valid_keys = []
-
-valid_keys = Parallel(n_jobs=-1)(delayed(process_key)(i) for i in cross_product)
+# controller_list = pd.unique(df['Controller'].ravel())
+# controller_list = np.delete(controller_list, np.nan)
+#
+# subcontroller_list = pd.unique(df['Subcontroller'].ravel())
+# subcontroller_list = np.delete(subcontroller_list, np.nan)
+#
+# pointname_list = pd.unique(df['PointName'].ravel())
+# pointname_list = np.delete(pointname_list, np.nan)
+#
+# cross_product = itertools.product(controller_list, subcontroller_list, pointname_list)
+#
+# valid_keys = []
+#
+# valid_keys = Parallel(n_jobs=-1)(delayed(process_key)(i) for i in cross_product)
 
 #for key in cross_product:
 #    ctrlr, sub_cntrlr, pt_nm = key
@@ -56,9 +53,10 @@ valid_keys = Parallel(n_jobs=-1)(delayed(process_key)(i) for i in cross_product)
 #     if tmp_dt.size:
 #         valid_keys[key] = tmp_dt.size
 
+groups = grouped.groups
 
-with open('/home/ashishgagneja/misc/valid_keys.pickle', 'wb+') as f:
-    pickle.dump(valid_keys, f)
+keys = list(groups.keys())
+pickle.dump(keys, open("/home/ashishgagneja/misc/group_keys.pickle", 'wb+'))
 
 store.close()
 
