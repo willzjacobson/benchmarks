9680c8dea8fe3eb721d37c1d239c750c0ce87859
David Karapetyan <dkarapetyan@prescriptivedata.io>
2015-11-24 16:21:59 -0500

    refactoring of frontend script code. no longer outputs txt file, just dictionary

--- TPO2-Rudin/frontend/frontend_conv.py
+++ TPO2-Rudin/frontend/frontend_conv.py
@@ -1,15 +1,16 @@
-__author__ = 'davidkarapetyan'
-
 import json
+
+__author__ = 'davidkarapetyan'
 
 
 def frontend_conv(ts):
-    temp = json.loads(ts.to_json(date_format="iso", orient="split"))
-    filename = "/data/" + ts.name + ".json"
+    """Converts Pandas time series to format specified by frontend team
 
+    :param ts: series object
+    :return: dictionary
+    """
+    temp = json.loads(ts.to_json(date_format="iso", orient="split"))
     ts_list = [{"time": time, "value": value} for time, value in
                zip(temp["index"], temp['data'])]
-    new_json = {'time_series': ts_list, 'name': temp['name']}
-    file = open(filename, "w")
-    json.dump(new_json, file)
-    file.close()
+    new_dict = {'time_series': ts_list, 'name': temp['name']}
+    return new_dict
