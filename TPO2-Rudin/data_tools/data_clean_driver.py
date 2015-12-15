e32750bee471782b893dbfa8f154aaac31a49a78
David Karapetyan <dkarapetyan@prescriptivedata.io>
2015-12-15 16:27:29 -0500

    deleted data_tools folder--all modules in it related to h5 file parsing, which we are no longer using in development code

--- TPO2-Rudin/data_tools/data_clean_driver.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-__author__ = 'ashishgagneja'
-
-import data_tools.data_clean
-
-if __name__ == "__main__":
-    data_tools.data_clean.csv_multicsv_to_hdf('/data/park345_nonempty_tables')
