6cf8ff9cc6e23102eaaffef60ee8b8dcdef580c7
David Karapetyan <dkarapetyan@prescriptivedata.io>
2016-01-04 14:39:37 -0500

    extended initialization

--- occupancy/run.py
+++ occupancy/run.py
@@ -2,9 +2,9 @@
 
 # import buildings_dbs as dbs
 import svm.model
-import ts_proc.utils
 from config import config
 from occupancy.svm_dframe_prep import get_covars
+from ts_proc.utils import get_occupancy_ts
 from weather.mongo import get_history, get_forecast
 
 ##########
@@ -12,10 +12,16 @@ dbs = config["building_dbs"]
 buildings = config["default"]["buildings"]
 
 for building in buildings:
-    endog = ts_proc.utils.get_occupancy_ts(**(dbs["mongo_cred"]),
-                                           **(dbs["weather_history_loc"]),
-                                           **(dbs["building_ts_loc"]),
-                                           )
+    endog = get_occupancy_ts(host=dbs["mongo_cred"]["host"],
+                             port=dbs["mongo_cred"]["port"],
+                             source=dbs["mongo_cred"]["source"],
+                             username=dbs["mongo_cred"][
+                                 "username"],
+                             db_name=dbs["building_ts_loc"][
+                                 "db_name"],
+                             collection_name=dbs["building_ts_loc"][
+                                 "collection_name"],
+                             building=)
 
     covars = get_covars(endog=endog,
                         gran=config.config["sampling"][
