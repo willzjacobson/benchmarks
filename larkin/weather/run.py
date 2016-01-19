6cae5151443bce163f4d4e91ca53a737991ee46d
davidkarapetyan <dkarapetyan@prescriptivedata.io>
2016-01-19 15:57:12 -0500

    weather_script now can be run by being imported into another module. Changed in order to run with scheduler package

--- larkin/weather/run.py
+++ larkin/weather/run.py
@@ -4,44 +4,43 @@ import larkin.weather.mongo
 from larkin.user_config import user_config
 
 cfg = user_config["building_dbs"]
-if __name__ == "__main__":
-    dbs = user_config["building_dbs"]
-    # whist = pd.read_hdf("/data/weather.h5", "history_orig")
-    # weather.mongo._mongo_history_push(whist, **(cfg["mongo_cred"]),
-    #                                   **(cfg["weather_history_loc"]))
+dbs = user_config["building_dbs"]
+# whist = pd.read_hdf("/data/weather.h5", "history_orig")
+# weather.mongo._mongo_history_push(whist, **(cfg["mongo_cred"]),
+#                                   **(cfg["weather_history_loc"]))
 
-    # bobo = weather.mongo.get_history(**(cfg["mongo_cred"]),
-    #                              **(cfg["weather_history_loc"]))
-    # hello = "string"
-    larkin.weather.mongo.history_update(
-            cap=90000000000,
-            parallel=True,
-            wund_url=dbs["wund_cred"]["wund_url"],
-            city=dbs["wund_cred"]["city"],
-            state=dbs["wund_cred"]["state"],
-            host=dbs["mongo_cred"]["host"],
-            port=dbs["mongo_cred"]["port"],
-            source=dbs["mongo_cred"]["source"],
-            username=dbs["mongo_cred"][
-                "username"],
-            password=dbs["mongo_cred"]["password"],
-            db_name=dbs["weather_history_loc"][
-                "db_name"],
-            collection_name=dbs["weather_history_loc"][
-                "collection_name"]
-    )
+# bobo = weather.mongo.get_history(**(cfg["mongo_cred"]),
+#                              **(cfg["weather_history_loc"]))
+# hello = "string"
+larkin.weather.mongo.history_update(
+        cap=90000000000,
+        parallel=True,
+        wund_url=dbs["wund_cred"]["wund_url"],
+        city=dbs["wund_cred"]["city"],
+        state=dbs["wund_cred"]["state"],
+        host=dbs["mongo_cred"]["host"],
+        port=dbs["mongo_cred"]["port"],
+        source=dbs["mongo_cred"]["source"],
+        username=dbs["mongo_cred"][
+            "username"],
+        password=dbs["mongo_cred"]["password"],
+        db_name=dbs["weather_history_loc"][
+            "db_name"],
+        collection_name=dbs["weather_history_loc"][
+            "collection_name"]
+)
 
-    larkin.weather.mongo.forecast_update(
-            wund_url=dbs["wund_cred"]["wund_url"],
-            city=dbs["wund_cred"]["city"],
-            state=dbs["wund_cred"]["state"],
-            host=dbs["mongo_cred"]["host"],
-            port=dbs["mongo_cred"]["port"],
-            source=dbs["mongo_cred"]["source"],
-            username=dbs["mongo_cred"][
-                "username"],
-            password=dbs["mongo_cred"]["password"],
-            db_name=dbs["weather_forecast_loc"][
-                "db_name"],
-            collection_name=dbs["weather_forecast_loc"][
-                "collection_name"])
+larkin.weather.mongo.forecast_update(
+        wund_url=dbs["wund_cred"]["wund_url"],
+        city=dbs["wund_cred"]["city"],
+        state=dbs["wund_cred"]["state"],
+        host=dbs["mongo_cred"]["host"],
+        port=dbs["mongo_cred"]["port"],
+        source=dbs["mongo_cred"]["source"],
+        username=dbs["mongo_cred"][
+            "username"],
+        password=dbs["mongo_cred"]["password"],
+        db_name=dbs["weather_forecast_loc"][
+            "db_name"],
+        collection_name=dbs["weather_forecast_loc"][
+            "collection_name"])
