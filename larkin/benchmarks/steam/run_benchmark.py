5c0a251072080f30faa0c65945fa0ab0eb38ef16
davidkarapetyan <dkarapetyan@prescriptivedata.io>
2016-01-19 16:34:16 -0500

    jobs file skeleton finished. Calls weather update run and benchmark run, respectively

--- larkin/benchmarks/steam/run_benchmark.py
+++ larkin/benchmarks/steam/run_benchmark.py
@@ -9,46 +9,53 @@ import datetime
 import sys
 
 import larkin.benchmarks.steam.benchmark
-import user_config
 import model_config
+import user_config
 
 cfg = dict(user_config.user_config, **model_config.model_config)
 
-# determine base date
-bench_dt = datetime.date.today()
 
-arg_count = len(sys.argv)
-if arg_count not in [1, 4]:
-    raise Exception("Usage: python %s [YYYY MM DD]"
-                    % sys.argv[0])
+def main():
+    # determine base date
+    bench_dt = datetime.date.today()
 
-elif arg_count == 4:
-    int_args = list(map(int, sys.argv[1:]))
-    bench_dt = datetime.date(int_args[0], int_args[1], int_args[2])
+    arg_count = len(sys.argv)
+    if arg_count not in [1, 4]:
+        raise Exception("Usage: python %s [YYYY MM DD]"
+                        % sys.argv[0])
 
-print("looking up benchmark for %s" % bench_dt)
+    elif arg_count == 4:
+        int_args = list(map(int, sys.argv[1:]))
+        bench_dt = datetime.date(int_args[0], int_args[1], int_args[2])
 
-# fill keyword argument dict
-kw_args = dict(dict(cfg['building_dbs']['mongo_cred'],
-                    **cfg['building_dbs']['building_ts_loc']),
-               **cfg['building_dbs']['results_steam_benchmark'])
+    print("looking up benchmark for %s" % bench_dt)
 
-weather_hist = cfg['building_dbs']['weather_history_loc']
-weather_fcst = cfg['building_dbs']['weather_forecast_loc']
+    # fill keyword argument dict
+    kw_args = dict(dict(cfg['building_dbs']['mongo_cred'],
+                        **cfg['building_dbs']['building_ts_loc']),
+                   **cfg['building_dbs']['results_steam_benchmark'])
 
-kw_args['weather_hist_db']         = weather_hist['db_name']
-kw_args['weather_hist_collection'] = weather_hist['collection_name']
-kw_args['weather_fcst_db']         = weather_fcst['db_name']
-kw_args['weather_fcst_collection'] = weather_fcst['collection_name']
+    weather_hist = cfg['building_dbs']['weather_history_loc']
+    weather_fcst = cfg['building_dbs']['weather_forecast_loc']
 
-kw_args['granularity'] = cfg['sampling']['granularity']
-kw_args['base_dt']     = bench_dt
-kw_args['debug']       = cfg['default']['debug']
+    kw_args['weather_hist_db'] = weather_hist['db_name']
+    kw_args['weather_hist_collection'] = weather_hist['collection_name']
+    kw_args['weather_fcst_db'] = weather_fcst['db_name']
+    kw_args['weather_fcst_collection'] = weather_fcst['collection_name']
 
-# iterate over all buildings
-for building_id in cfg['default']['buildings']:
+    kw_args['granularity'] = cfg['sampling']['granularity']
+    kw_args['base_dt'] = bench_dt
+    kw_args['debug'] = cfg['default']['debug']
 
-    bldg_params = cfg['default'][building_id]
-    larkin.benchmarks.steam.benchmark.process_building(building_id,
-                            timezone=bldg_params['timezone'],
-                            **kw_args)
+    # iterate over all buildings
+    for building_id in cfg['default']['buildings']:
+        bldg_params = cfg['default'][building_id]
+        larkin.benchmarks.steam.benchmark.process_building(
+                building_id,
+                timezone=bldg_params[
+                    'timezone'],
+                **kw_args)
+
+
+if __name__ == '__main__':
+    main()
--- larkin/jobs.py
+++ larkin/jobs.py
@@ -1,4 +1,25 @@
 # coding=utf-8
+import time
 
-def job():
-    pass
+import schedule
+
+import larkin.benchmarks.electric.run_benchmark as erb
+import larkin.benchmarks.steam.run_benchmark as srb
+import larkin.weather.run as wr
+
+
+def weather_update_job():
+    wr.main()
+
+
+def benchmark_run_job():
+    erb.main()
+    srb.main()
+
+
+schedule.every().hour.do(weather_update_job)
+schedule.every().hour.do(benchmark_run_job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
