7047a60290449f3819ff77e24b095cdadedf65a8
ashishgagneja <agagneja@prescriptivedata.io>
2016-01-25 11:15:05 -0500

    remove unnecessary functions

--- larkin/ts_proc/utils.py
+++ larkin/ts_proc/utils.py
@@ -11,13 +11,9 @@ import pymongo
 # from statsmodels.tsa.statespace.sarimax import SARIMAX
 # import seaborn as sns
 import pandas as pd
-import joblib
 # sns.set()
 import pytz
-import datetime
 
-#TODO: delete these import
-import misc
 
 def _construct_electric_dataframe(ts_lists, value_lists):
     """
@@ -41,54 +37,12 @@ def _construct_electric_dataframe(ts_lists, value_lists):
     if not len(ts_lists):
         return master_df
 
-
-    start_tm = datetime.datetime.utcnow()
-    # insert column data
-    for i, ts_list in enumerate(ts_lists):
-        print(len(value_lists[i]))
-        print(len(ts_list))
-        master_df = master_df.join(pd.DataFrame(data=value_lists[i],
-                                                index=ts_list,
-                                                columns=[str(i + 1)]).dropna(),
-                                   how='outer')
-
-    print("df join took: %s" % misc.get_elapsed_tm(start_tm))
-
-    return master_df.sum(axis=1).reset_index().drop_duplicates(
-        subset='index').set_index('index').sort_index()
-
-
-def _construct_electric_dataframe_new(ts_lists, value_lists, data):
-    """
-    Construct a pandas DataFrame using the time series data on individual
-    meter can compute the total instantaneous usage
-
-    :param ts_lists: list of lists
-        list of list of timestamps
-    :param value_lists: list of lists
-        list of list of data corresponding to the timestamps list in the same
-        ordinal position in ts_lists
-
-    :return:
-    """
-
-    master_df = pd.DataFrame()
-
-    # error checks
-    if len(ts_lists) != len(value_lists):
-        raise ValueError('array lengths must match')
-    if not len(ts_lists):
-        return master_df
-
-    start_tm = datetime.datetime.utcnow()
     # insert column data
     for i, ts_list in enumerate(ts_lists):
         master_df = master_df.join(pd.DataFrame(data=value_lists[i],
                                                 index=ts_list,
                                                 columns=[str(i + 1)]).dropna(),
                                    how='outer')
-
-    print("df join took: %s" % misc.get_elapsed_tm(start_tm))
 
     return master_df.sum(axis=1).reset_index().drop_duplicates(
         subset='index').set_index('index').sort_index()
@@ -121,37 +75,8 @@ def get_electric_ts(host, port, database, username, password, source,
     :return: pandas Dataframe
     """
 
-    start_tm = datetime.datetime.utcnow()
-    results = joblib.Parallel(n_jobs=-1)(joblib.delayed(get_ts_new_schema)(
-                    host, port, database, username, password, source,
-                    collection_name, building,
-                    ["Elec-M%d" % meter_num,
-                     "Electric_Meter_%d^Avg_Rate" % meter_num],
-                    ['SIF_Electric_Demand', 'Electric_Utility'])
-                        for meter_num in range(1, meter_count + 1))
-
-    print("multi-thread query took: %s" % misc.get_elapsed_tm(start_tm))
-
-    ts_lists, value_lists = zip(*results)
-
-    start_tm = datetime.datetime.utcnow()
-    ts_lists_st, value_lists_st = [], []
-    for meter_num in range(1, meter_count + 1):
-        ts_list_t, value_list_t = get_ts_new_schema(host, port, database,
-                                                    username, password, source,
-                                                    collection_name, building,
-                                                    ["Elec-M%d" % meter_num,
-                                                     "Electric_Meter_%d^Avg_Rate" % meter_num],
-                                                    ['SIF_Electric_Demand',
-                                                     'Electric_Utility'])
-        ts_lists_st.append(ts_list_t)
-        value_lists_st.append(value_list_t)
-
-    print("single-thread query took: %s" % misc.get_elapsed_tm(start_tm))
-
-    start_tm = datetime.datetime.utcnow()
-    devices = []
-    meter_groups = {}
+    devices, meter_groups = [], {}
+    ts_lists, value_lists = [], []
     for meter_num in range(1, meter_count + 1):
         meters_t = ["Elec-M%d" % meter_num,
                     "Electric_Meter_%d^Avg_Rate" % meter_num]
@@ -159,35 +84,22 @@ def get_electric_ts(host, port, database, username, password, source,
             meter_groups[meter_t] = meter_num - 1
         devices.extend(meters_t)
 
-    ts_list_t2, value_list_t2, device_data = get_ts_new_schema_electric(host, port, database,
-                                                    username, password, source,
-                                                    collection_name, building,
-                                                    devices,
-                                                    ['SIF_Electric_Demand',
-                                                     'Electric_Utility'])
-    ts_lists_t2, value_lists_t2 = [[], [], [], [], [], []], [[], [], [], [], [], []]
+        ts_lists.append([])
+        value_lists.append([])
+
+    device_data = get_device_ts_new_schema(host, port, database, username,
+                                             password, source, collection_name,
+                                             building, devices,
+                                             ['SIF_Electric_Demand',
+                                              'Electric_Utility'])
+
     for device, data in device_data.iteritems():
-        print("device data len: %s:%s" % (len(data[0]), len(data[1])))
         group = meter_groups[device]
-        print("device: %s, group: %s" % (device, group))
-        ts_lists_t2   [group].extend(data[0])
-        value_lists_t2[group].extend(data[1])
+        ts_lists   [group].extend(data[0])
+        value_lists[group].extend(data[1])
 
-    print("ts_lists_t2: %s, %s" % (len(ts_lists_t2), len(ts_lists_t2[1])))
-    print("value_lists_t2: %s, %s" % (len(value_lists_t2), len(value_lists_t2[1])))
-    print(meter_groups)
-
-    print("single-thread unified query took: %s" % misc.get_elapsed_tm(start_tm))
-        # ts_lists_st.append(ts_list_t)
-        # value_lists_st.append(value_list_t)
-
-    df0 = _construct_electric_dataframe(ts_lists, value_lists).tz_localize(
-                                                                       pytz.utc)
-    df1 = _construct_electric_dataframe(ts_lists_st, value_lists_st).tz_localize(
-                                                                       pytz.utc)
-    df2 = _construct_electric_dataframe(ts_lists_t2, value_lists_t2).tz_localize(
-        pytz.utc)
-    return df2
+    return _construct_electric_dataframe(ts_lists, value_lists).tz_localize(
+        pytz.utc)[0]
 
 
 def get_parsed_ts_new_schema(host, port, db_name, username, password,
@@ -224,11 +136,16 @@ def get_parsed_ts_new_schema(host, port, db_name, username, password,
         occupancy time series data
     """
 
-
-    ts_list, val_list = get_ts_new_schema(host, port, db_name, username,
-                                          password, source, collection_name,
-                                          building, devices, systems,
-                                          floor=floor, quad=quad)
+    device_data = get_device_ts_new_schema(host, port, db_name, username,
+                                           password, source, collection_name,
+                                           building, devices, systems,
+                                           floor=floor, quad=quad)
+    if not hasattr(devices, '__iter__'):
+        devices = [devices]
+    ts_list, val_list = [], []
+    for device in devices:
+        ts_list .extend(device_data[device][0])
+        val_list.extend(device_data[device][1])
 
     obs_df = pd.DataFrame({'obs': val_list}, index=ts_list).dropna()
 
@@ -238,21 +155,9 @@ def get_parsed_ts_new_schema(host, port, db_name, username, password,
         'index').sort_index().tz_localize(pytz.utc)['obs']
 
 
-def _procees_doc(doc):
-
-    readings = doc['readings']
-    zipped = [(x['time'], x['value']) for x in readings
-              if x['time'] is not None]
-
-    if len(zipped):
-        ts_list_t, val_list_t = zip(*zipped)
-        return [ts_list_t, val_list_t]
-    return [None, None]
-
-
-def get_ts_new_schema(host, port, database, username, password, source,
-                      collection_name, building, devices, systems=None,
-                      floor=None, quad=None):
+def get_device_ts_new_schema(host, port, database, username, password, source,
+                               collection_name, building, devices, systems=None,
+                               floor=None, quad=None):
     """
     Get all observation data with the given building, devices and systems
     combination from the database
@@ -282,7 +187,8 @@ def get_ts_new_schema(host, port, database, username, password, source,
     :param quad: string
         quadrant identifier
 
-    :return: tuple with a list of time stamps followed by a list of values
+    :return: device-indexed dictionary with  a list of lists of timestamps
+    followed by a list of values
     """
 
     with pymongo.MongoClient(host, port) as conn:
@@ -290,91 +196,7 @@ def get_ts_new_schema(host, port, database, username, password, source,
         conn[database].authenticate(username, password, source=source)
         collection = conn[database][collection_name]
 
-        ts_list, value_list = [], []
-
-        query = {"building": building}
-        # there may be one or more devices to match
-        if hasattr(devices, '__iter__'):
-                 query['device'] = {'$in': devices}
-        else:
-            query['device'] = devices
-
-       # systems is optional, for example for steam data
-        # handle optional arguments
-        for field, value in {'system': systems, 'floor': floor,
-                             'quadrant': quad}.iteritems():
-            if value:
-                # there may be one or more systems names to match
-                if hasattr(value, '__iter__'):
-                    query[field] = {'$in': value}
-                else:
-                    query[field] = value
-
-        # start_tm = datetime.datetime.utcnow()
-        for data in collection.find(query):
-
-            readings = data['readings']
-            zipped = [(x['time'], x['value']) for x in readings
-                                      if x['time'] is not None]
-
-            if len(zipped):
-                ts_list_t, val_list_t = zip(*zipped)
-                ts_list.extend(ts_list_t)
-                value_list.extend(val_list_t)
-
-        # print("old post-process took: %s" % misc.get_elapsed_tm(start_tm))
-
-
-        # start_tm = datetime.datetime.utcnow()
-        # results = map(_procees_doc, collection.find(query))
-        # [for ts_l, val_l in results if None not in [x,y]]
-        # ts_list_new, value_list_new = zip(*results)
-        # print("new post-process took: %s" % misc.get_elapsed_tm(start_tm))
-
-    return ts_list, value_list
-
-
-def get_ts_new_schema_electric(host, port, database, username, password, source,
-                      collection_name, building, devices, systems=None,
-                      floor=None, quad=None):
-    """
-    Get all observation data with the given building, devices and systems
-    combination from the database
-
-    :param host: string
-        database server name or IP-address
-    :param port: int
-        database port number
-    :param database: string
-        name of the database on server
-    :param username: string
-        database username
-    :param password: string
-        database password
-    :param source: string
-        source database for authentication
-    :param collection_name: string
-        database collection name
-    :param building: string
-        building identifier
-    :param devices: string or iterable
-        device name(s) for identifying time series
-    :param systems: string or iterable
-        system name(s) for identifying time series
-    :param floor: string
-        floor identifier
-    :param quad: string
-        quadrant identifier
-
-    :return: tuple with a list of time stamps followed by a list of values
-    """
-
-    with pymongo.MongoClient(host, port) as conn:
-
-        conn[database].authenticate(username, password, source=source)
-        collection = conn[database][collection_name]
-
-        ts_list, value_list, device_data = [], [], {}
+        device_data = {}
 
         query = {"building": building}
         # there may be one or more devices to match
@@ -386,7 +208,6 @@ def get_ts_new_schema_electric(host, port, database, username, password, source,
             query['device'] = devices
             device_data[devices] = [[], []]
 
-        print(device_data)
         # handle optional arguments
         # systems is optional, for example for steam data
         for field, value in {'system': systems, 'floor': floor,
@@ -398,7 +219,6 @@ def get_ts_new_schema_electric(host, port, database, username, password, source,
                 else:
                     query[field] = value
 
-        # start_tm = datetime.datetime.utcnow()
         for doc in collection.find(query):
 
             device = doc['device']
@@ -408,22 +228,10 @@ def get_ts_new_schema_electric(host, port, database, username, password, source,
 
             if len(zipped):
                 ts_list_t, val_list_t = zip(*zipped)
-                ts_list.extend(ts_list_t)
-                value_list.extend(val_list_t)
-
                 device_str = str(device)
                 device_data[device_str][0].extend(ts_list_t)
                 device_data[device_str][1].extend(val_list_t)
 
-                # print("old post-process took: %s" % misc.get_elapsed_tm(start_tm))
-
-
-                # start_tm = datetime.datetime.utcnow()
-                # results = map(_procees_doc, collection.find(query))
-                # [for ts_l, val_l in results if None not in [x,y]]
-                # ts_list_new, value_list_new = zip(*results)
-                # print("new post-process took: %s" % misc.get_elapsed_tm(start_tm))
-
-    return ts_list, value_list, device_data
+    return device_data
 
 
