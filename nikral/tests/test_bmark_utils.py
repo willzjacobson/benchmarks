07bbba0c8a56a5d2cdeab2b3e4756ee20947a5d6
ashishgagneja <agagneja@prescriptivedata.io>
2016-02-19 14:33:56 -0500

    added unit test for gen_bmark_readings_list

--- nikral/tests/test_bmark_utils.py
+++ nikral/tests/test_bmark_utils.py
@@ -7,6 +7,7 @@ import datetime
 
 import pytz
 import pandas as pd
+import numpy as np
 
 import nikral.benchmarks.utils as utils
 
@@ -16,8 +17,8 @@ class TestBmarkUtils(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.holidays = utils.gen_holidays(datetime.date(2012, 1, 1),
-                                          datetime.date(datetime.date.today().year,
-                                                        12, 31),
+                                          datetime.date(
+                                            datetime.date.today().year, 12, 31),
                                           '345_Park')
 
 
@@ -62,6 +63,47 @@ class TestBmarkUtils(unittest.TestCase):
         y, x = [5, 4, 9], [0, 1 ,3]
         self.assertTrue(([0.0, 4.5, 17.5], 17.5) == utils.incremental_trapz(y,
                                                                             x))
+
+
+    def _gen_ts(self, begin, end, gran):
+        test_idx = pd.DatetimeIndex(start=begin, end=end, freq="%dmin" % gran)
+        return pd.Series(data=np.random.rand(test_idx.size), index=test_idx)
+
+
+    def test_gen_data_availability_dates(self):
+        gran = 15
+        test_ts = self._gen_ts(datetime.datetime(2016, 1, 1, 2, 30),
+                               datetime.datetime(2016, 1, 2, 19),
+                               gran)
+        self.assertTrue({datetime.date(2016, 1, 1)} ==
+                        utils.get_data_availability_dates(test_ts, gran))
+
+
+    def test_gen_bmark_readings_list(self):
+        gran = 15
+        bench_dt = datetime.date(2016, 1, 1)
+        test_idx = pd.DatetimeIndex(start=datetime.datetime.combine(bench_dt,
+                                                        datetime.time(2, 30)),
+                                    end  =datetime.datetime.combine(bench_dt,
+                                                        datetime.time(4, 45)),
+                                    freq ="%dmin" % gran)
+
+        data = [5, 4, 7, 1, 2, 0, 3, 6, 8, 10]
+        auc  = [3, 6, 8, 11, 15, 21, 22, 27, 29, 30]
+        tzone = pytz.timezone('US/Central')
+        base_dt = datetime.date(2016, 2, 2)
+
+        test_ts = pd.Series(data=data,
+                            index=map(lambda x: x.time(),
+                                      test_idx.to_datetime()))
+        readings = utils.gen_bmark_readings_list(test_ts, auc, base_dt, tzone)
+
+        for t in readings:
+            tm, val, daily = t['time'], t['value'], t['daily']
+            tm_central = tm.astimezone(tzone)
+            idx = data.index(test_ts.loc[tm_central.time()])
+            self.assertTrue(data[idx] == val)
+            self.assertTrue(auc[idx] == daily)
 
 
 
