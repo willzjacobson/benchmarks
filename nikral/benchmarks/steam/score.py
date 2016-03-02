cf8c080ba0c3d7714816f36d9c2fba06caac7df8
ashishgagneja <agagneja@prescriptivedata.io>
2016-03-02 15:10:07 -0500

    updated version of timeseries data retrieval functions

--- nikral/benchmarks/steam/score.py
+++ nikral/benchmarks/steam/score.py
@@ -5,6 +5,7 @@ import sys
 
 import nikral.benchmarks.utils as butils
 from nikral.user_config import user_config
+import nikral.ts_proc.utils as ts_utils
 
 
 def main():
@@ -20,6 +21,7 @@ def main():
         # bldg_params = cfg['default'][building]
         # print(user_config['building_dbs']['mongo_cred'])
         bmark_cfg = user_config['building_dbs']['results_steam_benchmark']
+        # bldg_ts_cfg = user_config['building_dbs']['building_ts_loc']
 
         # get benchmark result
         bench_ts = butils.get_bench_ts(bench_dt=score_dt,
@@ -35,8 +37,20 @@ def main():
         print(bench_ts)
         print(butils.readings_to_ts(bench_ts))
 
+        kw_args = dict(user_config['building_dbs']['building_ts_loc'],
+                       **user_config['building_dbs']['mongo_cred'])
         # get actual
-        # ts_utils.get_date_ts(score_dt)
+        # ts_utils.get_date_ts(building=building,
+                             # database=bldg_ts_cfg['db_name'],
+                             # collection_name=bldg_ts_cfg['collection_name'],
+                             # dt=score_dt,
+                             # devices='TotalInstant',
+                             # **kw_args)
+        actual_ts = ts_utils.get_parsed_ts_new_schema1(building=building,
+                                                       devices='TotalInstant',
+                                                       ts_date=score_dt,
+                                                       **kw_args)
+        print(actual_ts)
 
         # score
 
