5867aa718ebc545888843d841308a01e9ed791d0
Will Jacobson <will@wills-imac.internal.prescriptivedata.io>
2016-04-05 11:58:29 -0400

    blagg

--- bash_scripts/after_install.sh
+++ bash_scripts/after_install.sh
@@ -4,8 +4,8 @@
 [[ -d /var/analytics/anaconda2 ]] || bash /var/analytics/Anaconda2-4.0.0-Linux-x86_64.sh -b -p /var/analytics/anaconda2
 
 # pip install ./an-benchmarks sometimes throws error without this
-apt-get install -y python-pandas
-apt-get install -y python-numpy
+pip install --upgrade pip
+# apt-get install -y python-numpy
 
 pip install /var/analytics/an_benchmarks
 pip uninstall -y nikral
