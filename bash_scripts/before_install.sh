895046019fb35b5e64775a094ac368f8d1eceade
Will Jacobson <will@wills-imac.internal.prescriptivedata.io>
2016-04-05 16:07:12 -0400

    yall

--- bash_scripts/before_install.sh
+++ bash_scripts/before_install.sh
@@ -1,3 +1,5 @@
 # if anaconda not yet installed, install it
-dir="/var/analytics/anaconda2"
-[[ -d "$dir" ]] || bash /var/analytics/Anaconda2-4.0.0-Linux-x86_64.sh -b -p /var/analytics/anaconda2
+
+if [ ! -d /var/analytics/anaconda2 ]; then
+	bash /var/analytics/Anaconda2-4.0.0-Linux-x86_64.sh -b -p /var/analytics/anaconda2
+fi
