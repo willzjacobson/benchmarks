4593e6141c052a3c841b760e349a168de98ad90c
Will Jacobson <will@wills-imac.internal.prescriptivedata.io>
2016-04-05 15:15:37 -0400

    yaya

--- bash_scripts/after_install.sh
+++ bash_scripts/after_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# if anaconda not yet installed, install it
-[[ -d /var/analytics/anaconda2 ]] || bash /var/analytics/Anaconda2-4.0.0-Linux-x86_64.sh -b -p /var/analytics/anaconda2
-
 pip install /var/analytics/an_benchmarks
 pip uninstall -y nikral
 pip install /var/analytics/an_benchmarks
---
+++ bash_scripts/before_install.sh
@@ -0,0 +1,2 @@
+# if anaconda not yet installed, install it
+[[ -d /var/analytics/anaconda2 ]] || bash /var/analytics/Anaconda2-4.0.0-Linux-x86_64.sh -b -p /var/analytics/anaconda2
