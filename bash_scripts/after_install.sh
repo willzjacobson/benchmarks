0951300652b94b74c5c586f92489d2a455c6e2c7
Will Jacobson <will@wills-imac.internal.prescriptivedata.io>
2016-04-05 16:37:04 -0400

    y

--- bash_scripts/after_install.sh
+++ bash_scripts/after_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#su -c pip install /var/analytics/an_benchmarks ubuntu
-#su -c pip uninstall -y nikral ubuntu 
-#su -c pip install /var/analytics/an_benchmarks ubuntu
 export "PATH=/var/analytics/anaconda2/bin:$PATH"
 pip install /var/analytics/an_benchmarks
 pip uninstall -y nikral
